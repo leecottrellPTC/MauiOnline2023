d96fb8bd0551de41c5cf5a76a283bcb86879675c
Lee Cottrell <cottrell.lee@ptcollege.edu>
2023-10-11 18:52:22 -0400

    payroll app

---
+++ Week 1/Payroll/Payroll/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Payroll;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Week 1/Payroll/Payroll/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Payroll;
+
+public partial class MainPage : ContentPage
+{
+	double payrate = 27.00;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void CalcButton_Clicked(object sender, EventArgs e)
+    {
+		//pull from the screen
+		double hours = 0;
+		try
+		{
+			hours = Convert.ToDouble(HoursEntry.Text);
+		}
+		catch (Exception)
+		{
+			hours = 0;
+            DisplayAlert("Error", "Enter numbers only", "Bummer");
+
+            //title, message,                button
+            HoursEntry.Focus();	//focus is the object selected by the user
+								//putrs the cursor into the box
+			throw;
+		}
+		//should also surround with try catch
+		//do something with item on screen
+		double pay = hours * payrate;
+		//display outpout
+		PayLabel.Text = pay.ToString("$0.00");
+        
+    }
+}
+
